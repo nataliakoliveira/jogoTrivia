005267c20cac5e56ada9558b2e026b1e02dedffc
nataliakoliveira <nataliaakellyy@gmail.com>
2022-10-09 12:36:28 -0300

    requisito5

--- src/redux/action/index.js
+++ src/redux/action/index.js
@@ -1,8 +1,20 @@
 export const TYPE_API = 'TYPE_API';
+export const TYPE_LOGIN = 'TYPE_LOGIN';
+export const TYPE_SCORE = 'TYPE_SCORE';
 
 export const typeApi = (payload) => ({
   type: TYPE_API,
   payload,
+});
+
+export const typeLogin = (userData) => ({
+  type: TYPE_LOGIN,
+  payload: userData,
+});
+
+export const typeScore = (score) => ({
+  type: TYPE_SCORE,
+  score,
 });
 
 export const api = async () => {
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,15 +1,28 @@
-import { TYPE_API } from '../action';
+import { TYPE_SCORE, TYPE_LOGIN } from '../action';
 
 const INITIAL_STATE = {
   email: '',
+  name: '',
+  score: 0,
 };
 
 const user = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-  case TYPE_API:
+  /*   case TYPE_API:
     return {
       ...state,
       email: action.payload,
+    }; */
+  case TYPE_LOGIN:
+    return {
+      ...state,
+      email: action.payload.email,
+      name: action.payload.name,
+    };
+  case TYPE_SCORE:
+    return {
+      ...state,
+      score: action.score,
     };
   default:
     return state;
