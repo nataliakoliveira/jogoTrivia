b26bc1c849009ae8911a6698970fecc570456ca7
Cristiano <weydsoncristiano@hotmail.com>
2022-10-10 15:27:05 -0300

    correção req5

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,9 +31,9 @@ Header.propTypes = {
 };
 
 const mapStateToProps = (state) => ({
-  score: state.score,
-  name: state.name,
-  email: state.email,
+  score: state.user.score,
+  name: state.user.name,
+  email: state.user.email,
 });
 
 export default connect(mapStateToProps)(Header);
