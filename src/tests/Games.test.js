850cf5880021b768f3858d6dd1c58235e795c6d5
Cristiano <weydsoncristiano@hotmail.com>
2022-10-12 13:21:50 -0300

    adiantando alguns testes de cobertura game

--- src/tests/Games.test.js
+++ src/tests/Games.test.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux'
 import App from '../App';
-import { screen } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 
 describe('Teste o componente <App.js />', () => {
-  test('Testando caminho login a headers e  o HeadersGames.', async () => {
-    
-    const { history } = renderWithRouterAndRedux(<App />, { initialEntries: ['/game'] });
+  test('Testando a pagina de Games.', async () => {
+    const { history } = renderWithRouterAndRedux(<App />);
     const inputName = screen.getByTestId('input-player-name');
     userEvent.type(inputName, 'Rejane');
     expect(inputName.value).toBe('Rejane');
-
     const inputEmail = screen.getByTestId('input-gravatar-email');
     userEvent.type(inputEmail, "rejane@rejane.com");
     expect(inputEmail.value).toBe('rejane@rejane.com');
-
     const buttonPlay = screen.getByTestId('btn-play');
     userEvent.click(buttonPlay)
-
     const gameTexto = await screen.findByText('Página do Game')
     const { pathname } = history.location;
     expect(pathname).toBe('/game');
@@ -35,6 +31,24 @@ describe('Teste o componente <App.js />', () => {
     expect(scoreId).toBeInTheDocument();
     expect(pathname).toBe('/game');
     expect(gameTexto).toBeInTheDocument();
+    await waitFor(() => {
+      const perguntasId = screen.getByTestId('question-category');
+      expect(perguntasId).toBeInTheDocument();
 
-  });
+      const h1Timer =screen.getByRole('heading', {
+        name: /0/i
+      })
+      expect(h1Timer).toBeInTheDocument();
+      
+      const timerTela =screen.getByTestId('timertest');
+      expect(timerTela).toBeInTheDocument();
+      const categoryId = screen.getByTestId('question-text');
+      expect(categoryId).toBeInTheDocument();
+      const questionId = screen.getByTestId('answer-options');
+      expect(questionId).toBeInTheDocument();
+      const buttonAcert = screen.getByTestId('correct-answer')
+      expect(buttonAcert).toBeInTheDocument()
+    }, { timeout: 10000 })
+
+  })
 })
--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,45 +1,41 @@
 import React from 'react';
 import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux'
 import App from '../App';
-import { screen, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 
 describe('Teste o componente <App.js />', () => {
-  test('Verificação dos da pagina Login e pagina Gamer', async () => {
+  test('Testando caminho login a headers e  o HeadersGames.', async () => {
+    
     const { history } = renderWithRouterAndRedux(<App />);
     const inputName = screen.getByTestId('input-player-name');
     userEvent.type(inputName, 'Rejane');
     expect(inputName.value).toBe('Rejane');
+
     const inputEmail = screen.getByTestId('input-gravatar-email');
     userEvent.type(inputEmail, "rejane@rejane.com");
     expect(inputEmail.value).toBe('rejane@rejane.com');
+
     const buttonPlay = screen.getByTestId('btn-play');
     userEvent.click(buttonPlay)
+
     await waitFor(() => {
     const gameTexto = screen.getByText('Página do Game')
     const { pathname } = history.location;
     expect(pathname).toBe('/game');
     expect(gameTexto).toBeInTheDocument();
+
+    const nomeDaPaginaTela = screen.getByRole('heading', {
+      name: /Página do game/i
+    })
+    expect(nomeDaPaginaTela).toBeInTheDocument();
+    const imageGravatarId = screen.getByTestId('header-player-name')
+    expect(imageGravatarId).toBeInTheDocument();
+    const scoreId = screen.getByTestId('header-score')
+    expect(scoreId).toBeInTheDocument();
+    expect(pathname).toBe('/game');
+    expect(gameTexto).toBeInTheDocument();
   }, { timeout: 5000 })
-  });
-})
-
-describe('Teste o componente <App.js />', () => {
-  test('Verificação dos da pagina Settings', () => {
-    renderWithRouterAndRedux(<App />);
-    const inputName = screen.getByTestId('input-player-name');
-    userEvent.type(inputName, 'Rejane');
-    expect(inputName.value).toBe('Rejane');
-
-    const inputEmail = screen.getByTestId('input-gravatar-email');
-    userEvent.type(inputEmail, "rejane@rejane.com");
-    expect(inputEmail.value).toBe('rejane@rejane.com');
-
-    const buttonPlay = screen.getByTestId('btn-play');
-    userEvent.click(buttonPlay)
-    const buttonSettings = screen.getByTestId('btn-settings');
-    userEvent.click(buttonSettings);
-
   });
 })
